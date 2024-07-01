aa0beee754a9ad1ea5ed5afed243ef3691dadd61
Jiawei Wu <robertwu1996@gmail.com>
2024-07-01 15:01:59 +0800

    add two dimension array

---
+++ src/LearnArray/twoDimArray.java
@@ -0,0 +1,24 @@
+package LearnArray;
+
+// two dimension array
+
+public class twoDimArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3},
+        };
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
