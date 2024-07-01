ff2e5af246802592787441666d2b1d79831c8901
Jiawei Wu <robertwu1996@gmail.com>
2024-07-01 11:59:40 +0800

    add array practice2

---
+++ src/LearnArray/practice2.java
@@ -0,0 +1,19 @@
+package LearnArray;
+
+// copy an array to an new array
+
+import java.util.Arrays;
+
+public class practice2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int[] newArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(newArr));
+    }
+}
