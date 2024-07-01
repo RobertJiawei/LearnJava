18a8dc0a5cc51543cebb7d3a417ecb2512d0f978
Jiawei Wu <robertwu1996@gmail.com>
2024-07-01 11:54:06 +0800

    add array practice1

--- src/LearnArray/practice1.java
+++ src/LearnArray/practice1.java
@@ -3,6 +3,7 @@ package LearnArray;
 //random generate a verify code. code format is 4 letters plus a number.
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class practice1 {
     public static void main(String[] args) {
@@ -15,5 +16,16 @@ public class practice1 {
                 chs[i] = (char) (65 + i - 26); //from 'A' to 'Z'
             }
         }
+
+        String result = "";
+
+        Random r = new Random();
+        for (int i = 0; i < 4; i++) {
+            result = result + chs[r.nextInt(chs.length)];
+        }
+
+        result = result + r.nextInt(9);
+
+        System.out.println(result);
     }
 }
